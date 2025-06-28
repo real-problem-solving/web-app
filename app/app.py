e461b6379c12f6b0e4189c5fa758c763134c292c
Ramjan Raeen <ramjanraeen@gmail.com>
2025-06-28 12:30:42 +0530

    post request created

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import psycopg2
 
 app = Flask(__name__)
@@ -7,6 +7,13 @@ stores = [{"name": "My store", "items": [{"name": "My item", "price": 15.99}]}]
 @app.get('/store')
 def get_store():
     return {"stores": stores}
+@app.post('/store')
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+        
+    return {"store": new_store, "message": "Store created successfully"}, 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
